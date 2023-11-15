ac6ad073f65fdd3518645b01cef0d309291559a6
Leonardo-Tozato <leo.muniztozato@gmail.com>
2023-11-15 18:49:14 -0300

    feat: implement main

---
+++ cmd/app/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"ports-exercise/m/internal/handlers/portshdl"
+	"ports-exercise/m/internal/port"
+	"ports-exercise/m/internal/repositories/portsrepo"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <file_path>")
+		return
+	}
+	filePath := os.Args[1]
+
+	// Set up signal handling
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+	portsRepository := portsrepo.NewMemDB()
+	portsService := port.NewService(portsRepository)
+	portsHandler := portshdl.NewJsonHandler(portsService)
+
+	result, err := portsHandler.HandleUpsertStream(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	b, err := json.Marshal(result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
+
+	<-shutdown
+	fmt.Println("DONE")
+}
